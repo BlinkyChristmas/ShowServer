35ad1642e6647431c2446e58a825be12a4ee5b43
Charles Kerr <Something@mail.com>
2025-11-22 09:04:18 -0500

    startup

--- beagle/startup-service/showserver.sh
+++ beagle/startup-service/showserver.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+SLEEPSEC=2
+MAXLOOP=35
+loopcount=0
+PRUREADY=0
 
+sleep  50
 /root/startup/ShowServer /media/server.cfg 1>/dev/null 2>/dev/null
-# shutdown -h now
+
+FILE_PATH="/media/showshutdown.sh"
+
+if [ -f "$FILE_PATH" ]; then
+
+    shutdown -h now
+
+fi
